b6253a0c76fff410ed48acf30c260c04acfa977e
mashoesmith <145067465+mashoesmith@users.noreply.github.com>
2024-01-31 20:20:55 +0000

    portfolio layout

--- components/SearchProjects/Results/Results.js
+++ components/SearchProjects/Results/Results.js
@@ -2,7 +2,7 @@ import { ProjectCard } from "./ProjectCard";
 
 export const Results = ({ projects }) => {
   return (
-    <div className="max-w-5xl w-full px-6 mx-auto grid grid-cols-2 gap-3 justify-items-center my-6">
+    <div className="max-w-5xl w-full px-6 mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 justify-items-center my-6">
       {projects.map((project) => (
         <ProjectCard
           key={project.databaseId}
