204cf6a8bfdbbc4e379b1c5c1674b2c6111fd4ce
mashoesmith <145067465+mashoesmith@users.noreply.github.com>
2024-04-02 10:42:29 +0100

    my SVG spinner

--- components/HomePage/HomePage.js
+++ components/HomePage/HomePage.js
@@ -234,24 +234,38 @@ export const HomePage = () => {
         //     preloader = el;
         //   }}
         // ></div>
-        <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
-          <circle
+        // <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+        //   <circle
+        //     ref={(el) => {
+        //       preloader = el;
+        //     }}
+        //     className="spin"
+        //     cx="400"
+        //     cy="400"
+        //     fill="none"
+        //     r="20"
+        //     stroke-width="2"
+        //     stroke="#cccccc"
+        //     stroke-dasharray="90 140"
+        //     stroke-linecap="round"
+        //     // width="40px"
+        //     // height="40px"
+        //   />
+        // </svg>
+        <div id="spinnerContainer">
+          <svg
             ref={(el) => {
               preloader = el;
             }}
-            className="spin"
-            cx="400"
-            cy="400"
-            fill="none"
-            r="20"
-            stroke-width="2"
-            stroke="#cccccc"
-            stroke-dasharray="90 140"
-            stroke-linecap="round"
-            // width="40px"
-            // height="40px"
-          />
-        </svg>
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 179.44 179.44"
+          >
+            <path
+              class="spinner spin"
+              d="M165.26,46.11c7.43,12.83,11.68,27.72,11.68,43.61,0,48.18-39.05,87.23-87.22,87.23S2.5,137.9,2.5,89.72,41.55,2.5,89.72,2.5"
+            />
+          </svg>
+        </div>
       ) : (
         <>
           <img
