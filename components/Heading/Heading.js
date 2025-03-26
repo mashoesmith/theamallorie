536ff245bfedfc13982acce2acbe380e614eb5ff
mashoesmith <145067465+mashoesmith@users.noreply.github.com>
2025-03-26 15:36:31 +0000

    normalMargin

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { getFontSizeForHeading, getTextAlign } from "utils/fonts";
 
-export const Heading = ({ textAlign, textColor, content, level = 2 }) => {
+export const Heading = ({
+  textAlign,
+  textColor,
+  content,
+  level = 2,
+  classNames,
+}) => {
   const tag = React.createElement(`h${level}`, {
     dangerouslySetInnerHTML: { __html: content },
-    className: `font-heading max-w-5xl px-6 mx-auto mt-14 mb-5 md:mt-5 md:mb-5 ${getFontSizeForHeading(
+    className: `font-heading max-w-5xl px-6 mx-auto mt-14 mb-5 md:mt-5 md:mb-5 ${classNames} ${getFontSizeForHeading(
       level
     )} ${getTextAlign(textAlign)}`,
     style: { color: textColor },
