986b9b7cb284f6c44e3a64a117ccec92b0f9dd17
mashoesmith <145067465+mashoesmith@users.noreply.github.com>
2025-03-26 15:28:05 +0000

    mt-14

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -4,7 +4,7 @@ import { getFontSizeForHeading, getTextAlign } from "utils/fonts";
 export const Heading = ({ textAlign, textColor, content, level = 2 }) => {
   const tag = React.createElement(`h${level}`, {
     dangerouslySetInnerHTML: { __html: content },
-    className: `font-heading max-w-5xl px-6 mx-auto mt-10 mb-5 md:mt-5 md:mb-5 ${getFontSizeForHeading(
+    className: `font-heading max-w-5xl px-6 mx-auto mt-14 mb-5 md:mt-5 md:mb-5 ${getFontSizeForHeading(
       level
     )} ${getTextAlign(textAlign)}`,
     style: { color: textColor },
