a9227b6efc3ba870bebb06457fae8b479c7b69fb
mashoesmith <145067465+mashoesmith@users.noreply.github.com>
2025-12-04 14:16:46 +0000

    logs on getmenu

--- utils/mapMainMenuItems.js
+++ utils/mapMainMenuItems.js
@@ -1,15 +1,17 @@
 import { v4 as uuid } from "uuid";
 
 export const mapMainMenuItems = (menuItems) => {
-  return menuItems.map((menuItem) => ({
-    id: uuid(),
-    destination: menuItem.menuItem.destination?.url || null,
-    label: menuItem.menuItem.destination?.title,
-    target: menuItem.menuItem.destination?.target,
-    // subMenuItems: (menuItem?.items || []).map((subMenuItem) => ({
-    //   id: uuid(),
-    //   destination: subMenuItem?.destination?.uri,
-    //   label: subMenuItem?.label,
-    // })),
-  }));
+  if (!Array.isArray(menuItems)) {
+    console.error("menuItems is not an array:", menuItems);
+    return [];
+  }
+
+  return menuItems
+    .filter((item) => item?.menuItem?.destination)
+    .map((menuItem) => ({
+      id: uuid(),
+      destination: menuItem.menuItem.destination?.url || "/",
+      label: menuItem.menuItem.destination?.title || "Untitled",
+      target: menuItem.menuItem.destination?.target || "",
+    }));
 };
