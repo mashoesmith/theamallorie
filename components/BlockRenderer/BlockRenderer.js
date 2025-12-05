661ee274eb72c8769be232c892927df4b1260f09
mashoesmith <145067465+mashoesmith@users.noreply.github.com>
2025-12-05 12:13:16 +0000

    core buttons added instead

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -39,16 +39,38 @@ export const BlockRenderer = ({ blocks }) => {
           />
         );
       }
-      case "acf/ctabutton": {
+      // case "acf/ctabutton": {
+      //   return (
+      //     <CallToActionButton
+      //       key={block.id}
+      //       buttonLabel={block.attributes?.data.label}
+      //       destination={block.attributes?.data.destination.url || "/"}
+      //       tab={block.attributes?.data.destination.target}
+      //       align={block.attributes?.data.align}
+      //       classNames={block.attributes?.className}
+      //     />
+      //   );
+      // }
+
+      case "core/buttons": {
         return (
-          <CallToActionButton
-            key={block.id}
-            buttonLabel={block.attributes?.data.label}
-            destination={block.attributes?.data.destination.url || "/"}
-            tab={block.attributes?.data.destination.target}
-            align={block.attributes?.data.align}
-            classNames={block.attributes?.className}
-          />
+          <div className={block.attributes?.className}>
+            {block.innerBlocks?.map((inner) => {
+              if (inner.name === "core/button") {
+                return (
+                  <CallToActionButton
+                    key={inner.id}
+                    buttonLabel={inner.attributes?.content}
+                    destination={inner.attributes?.url || "/"}
+                    tab={inner.attributes?.linkTarget || "_blank"}
+                    align={inner.attributes?.align || "center"}
+                    classNames={inner.attributes?.className}
+                  />
+                );
+              }
+              return null;
+            })}
+          </div>
         );
       }
 
